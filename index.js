e05176393d4e09dec60cb72d4d69eff446cd48a3
shenowen36 <shenowen36@gmail.com>
2020-04-21 22:45:27 -0500

    Working version of fetch to api url

--- index.js
+++ index.js
@@ -6,7 +6,30 @@ addEventListener('fetch', event => {
  * @param {Request} request
  */
 async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
-  })
+  const url = "https://cfw-takehome.developers.workers.dev/api/variants"
+  
+  let response = await fetch(url);
+  const data = await response.json();
+  const variants = data.variants;
+  console.log(variants);
+
+  // 
+  
+  return fetch(url);
+
+  // return new Response('Hello worker!', {
+  //   headers: { 'content-type': 'application/json' },
+  // })
 }
+
+// fetch()
+//     .then(function(response) {
+//       //response.setHeader("Set-Cookie", "HttpOnly;Secure;SameSite=Strict");
+//       return response.json();
+//     })
+//     .then(function(data){
+//       //console.log(data);
+//       for(const variant of data.variants){
+//         console.log(variant);
+//       }
+//     })
